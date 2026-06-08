51616e7618ff22926d71147d877d19c1e874fb48
jayhuang30 <56987037+jayhuang30@users.noreply.github.com>
2026-06-08 18:25:32 +0800

    Create Good-Ex4-6.cs

---
+++ Examples/Ex4/Good-Ex4-6.cs
@@ -0,0 +1,12 @@
+[Theory]
+[InlineData("0,0,0", 0)]
+[InlineData("0,1,2", 3)]
+[InlineData("1,2,3", 6)]
+public void Add_MultipleNumbers_ReturnsSumOfNumbers(string input, int expected)
+{
+    var stringCalculator = new StringCalculator();
+
+    var actual = stringCalculator.Add(input);
+
+    Assert.Equal(expected, actual);
+}
